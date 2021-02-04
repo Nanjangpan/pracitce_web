a1adb31e6be21f5cde457a9a8f0954104ae7aeae
Nanjangpan <b970311@yonsei.ac.kr>
2021-02-04 08:34:37 +0000

    feat: nextjs, firebase setting

---
+++ packages/web/.next/static/chunks/pages/firebase-622ca7369182d6916cc5.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[7],{bxE0:function(_,n,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/firebase",function(){return w("03bF")}])}},[["bxE0",0,2]]]);
---
+++ packages/web/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import firebase from "../firebase";
+
+console.log(firebase)
+
+function Auth() {
+  return <div>Welcome to Next.js!</div>
+}
+
+export default Auth
+
